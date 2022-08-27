990b49bb7696696498a0be0f19f7ab0e20b49a1a
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-08-27 11:01:51 +0500

    1. Write a ts program to find maximum between two numbers

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ var form = document.querySelector('.inputs');
 var num01 = document.querySelector('#num01');
 var num02 = document.querySelector('#num02');
 /* The function that will calculate our inputs */
-function calculate(x, y) {
-    return x + y;
+function maxNumber(x, y) {
+    if (x > y) {
+        return x;
+    }
+    else {
+        return y;
+    }
 }
 /* Creat Event lisytener that will react on submit the button */
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     /* Print the output in cosole */
-    var sum = calculate(num01.valueAsNumber, num02.valueAsNumber);
-    console.log('Sum of two numbers = ', sum);
+    var max = maxNumber(num01.valueAsNumber, num02.valueAsNumber);
+    console.log('The Greater number is ', max);
 });
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,19 @@ var num01 = document.querySelector('#num01')! as HTMLInputElement
 var num02 = document.querySelector('#num02')! as HTMLInputElement 
 
 /* The function that will calculate our inputs */
-function calculate(x:number , y:number){
-    return x + y ;
+function maxNumber(x:number , y:number){
+    if(x > y){
+        return x 
+    }
+    else{
+        return y
+    } 
 }
 
 /* Creat Event lisytener that will react on submit the button */
 form.addEventListener("submit", (e:Event) => {
     e.preventDefault()
     /* Print the output in cosole */
-    var sum = calculate(num01.valueAsNumber, num02.valueAsNumber)
-    console.log('Sum of two numbers = ', sum)
+    var max = maxNumber(num01.valueAsNumber, num02.valueAsNumber)
+    console.log('The Greater number is ', max)
 })
