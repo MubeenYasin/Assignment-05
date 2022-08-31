7cf0ddbd2758efcb2df5ac2f716fc8a53be57dad
Mubeen Yasin <65570764+MubeenYasin@users.noreply.github.com>
2022-08-31 18:31:40 +0500

    Problem 01 to 10 is solved

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var form = document.querySelector('.inputs');
 var num01 = document.querySelector('#num01');
 var num02 = document.querySelector('#num02');
 /* ---------------------------------------------*/
-/* Function to find maximum between two numbers.*/
+/* 01. Write a ts program to find maximum between two numbers.*/
 /* ---------------------------------------------*/
 function maxNumberBT2(x, y) {
     if (x > y) {
@@ -25,7 +25,7 @@ form.addEventListener("submit", function (e) {
     console.log(max);
 });
 /* ---------------------------------------------*/
-/* Function to find maximum between three numbers.*/
+/* 02. Write a ts program to find maximum between three numbers.*/
 /* ---------------------------------------------*/
 /* Get form Elements*/
 var form02 = document.querySelector('.inputs-2');
@@ -67,7 +67,7 @@ form02.addEventListener("submit", function (e) {
     console.log(max3);
 });
 /* ---------------------------------------------*/
-/* program to check whether a number is negative, positive or zero.*/
+/* 03. program to check whether a number is negative, positive or zero.*/
 /* ---------------------------------------------*/
 var problem03 = document.querySelector('#problem03');
 var numPoN = document.querySelector("#PoN");
@@ -85,11 +85,32 @@ function postiveNumber(a) {
 problem03.addEventListener("submit", function (e) {
     e.preventDefault();
     /* Print the output in cosole */
-    var pos = postiveNumber(numPoN.valueAsNumber);
-    console.log(pos);
+    postiveNumber(numPoN.valueAsNumber);
 });
 /* ---------------------------------------------*/
-/* 4. Write a ts program to check whether a number is divisible by 5 and 11 or not.*/
+/* 05. 5. Write a ts program to check whether a number is even or odd.*/
+/* ---------------------------------------------*/
+var problem05 = document.querySelector('#problem05');
+var input05 = document.querySelector('#input05');
+function isEvenOdd(x) {
+    if (x !== 0) {
+        if (x % 2 == 0) {
+            return (console.log("The Number (".concat(x, ") is EVEN")));
+        }
+        else {
+            console.log("The Number (".concat(x, ") is ODD"));
+        }
+    }
+    else {
+        console.log('Please type a number greater than 0');
+    }
+}
+problem05.addEventListener("submit", function (e) {
+    e.preventDefault();
+    isEvenOdd(input05.valueAsNumber);
+});
+/* ---------------------------------------------*/
+/* 04. Write a ts program to check whether a number is divisible by 5 and 11 or not.*/
 /* ---------------------------------------------*/
 var problem04 = document.querySelector('#problem04');
 var divideTo = document.querySelector('#divideTo');
@@ -104,10 +125,10 @@ function divisible(x, y) {
 }
 problem04.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log(divisible(divideTo.valueAsNumber, divideBy.valueAsNumber));
+    divisible(divideTo.valueAsNumber, divideBy.valueAsNumber);
 });
 /* ---------------------------------------------*/
-/* 6. Write a ts program to check whether a year is leap year or not. */
+/* 06. Write a ts program to check whether a year is leap year or not. */
 /* ---------------------------------------------*/
 var problem06 = document.querySelector('#problem06');
 var year = document.querySelector('#year');
@@ -123,3 +144,141 @@ problem06.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(leapYear(year.valueAsNumber));
 });
+/* ---------------------------------------------*/
+/* 07. Write a ts program to check whether a character is alphabet or not. */
+/* ---------------------------------------------*/
+var problem07 = document.querySelector('#problem07');
+var character = document.querySelector('#character');
+function checkCharacter(x) {
+    if (x == x.toUpperCase()) {
+        return (console.log("The Character (".concat(x, ") is in Uppercase")));
+    }
+    else if (x == x.toLowerCase()) {
+        return (console.log("The Character (".concat(x, ") is in Lowercase")));
+    }
+    else {
+        return (console.log("This is not a Character. Please type a Character"));
+    }
+}
+problem07.addEventListener("submit", function (e) {
+    e.preventDefault();
+    checkCharacter(character.value);
+});
+/* ---------------------------------------------*/
+/* 08. Write a ts program to input any alphabet and check whether it is vowel or consonant.*/
+/* ---------------------------------------------*/
+var problem08 = document.querySelector('#problem08');
+var input08 = document.querySelector('#input08');
+function isVowel(x) {
+    var charLen = x.length;
+    var char;
+    var vowels = "aeiouAEIOU";
+    // console.log('Charactor Lenth', charLen);
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in vowels) {
+            if (vowels[char] == x) {
+                // console.log(n.length);
+                var vowelOut = vowels[char];
+                console.log("The Charactor (".concat(vowelOut, ") is a VOWEL Charactor"));
+                break;
+            }
+        }
+        if (!(vowelOut)) {
+            console.log("The Charactor (".concat(x, ") is not a VOWEL Charactor"));
+        }
+    }
+    else {
+        console.log('Please Write only one Charactor');
+    }
+}
+problem08.addEventListener("submit", function (e) {
+    e.preventDefault();
+    isVowel(input08.value);
+});
+/* ---------------------------------------------*/
+/* 09. Write a ts program to input any character and check whether it is alphabet, digit or special character.*/
+/* ---------------------------------------------*/
+var problem09 = document.querySelector('#problem09');
+var input09 = document.querySelector('#input09');
+function whatChar(x) {
+    var charLen = x.length;
+    var char;
+    var alphbets = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    // var spacialChar: any = "0123456789";
+    // console.log('Charactor Lenth', charLen);
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in alphbets) {
+            if (alphbets[char] == x) {
+                // console.log(n.length);
+                var alphbetOut = alphbets[char];
+                console.log("The Charactor (".concat(alphbetOut, ") is an Alphabet"));
+                break;
+            }
+        }
+        for (char in numbers) {
+            if (numbers[char] == x) {
+                // console.log(n.length);
+                var numberOut = numbers[char];
+                console.log("The Charactor (".concat(numberOut, ") is a NUMBER"));
+                break;
+            }
+        }
+        if (!(alphbetOut || numberOut)) {
+            console.log("The input (".concat(x, ") is a Spacial Charactor "));
+        }
+    }
+    else {
+        console.log('Please Write only one Charactor');
+    }
+}
+problem09.addEventListener("submit", function (e) {
+    e.preventDefault();
+    whatChar(input09.value);
+});
+/* ---------------------------------------------*/
+/* 10. Write a ts program to check whether a character is uppercase or lowercase alphabet. */
+/* ---------------------------------------------*/
+var problem07B = document.querySelector('#problem07B');
+var characterB = document.querySelector('#characterB');
+function isLowerUpper(x) {
+    var charLen = x.length;
+    var char;
+    var lower = "abcdefghijklmnopqrstuvqxyz";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // console.log('Charactor Lenth', charLen);
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in lower) {
+            if (lower[char] == x) {
+                // console.log(n.length);
+                var lowerOut = lower[char];
+                console.log("The Charactor (".concat(lowerOut, ") is an Alphabet and in LOWERCASE"));
+                break;
+            }
+        }
+        for (char in upper) {
+            if (upper[char] == x) {
+                // console.log(n.length);
+                var upperOut = upper[char];
+                console.log("The Charactor (".concat(upperOut, ") is an Alphabet and in UPPERCASE"));
+                break;
+            }
+        }
+        if (!(lowerOut || upperOut)) {
+            console.log("The input is not a charactor (".concat(x, "), Please Write a Alphabet charactor"));
+        }
+    }
+    else {
+        console.log('Please Write only one Charactor');
+    }
+}
+problem07B.addEventListener("submit", function (e) {
+    e.preventDefault();
+    isLowerUpper(characterB.value);
+});
+/* ---------------------------------------------*/
+/* 11. Write a ts program to input week number and print week day.*/
+/* ---------------------------------------------*/
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ var num01 = document.querySelector('#num01')! as HTMLInputElement
 var num02 = document.querySelector('#num02')! as HTMLInputElement
 
 /* ---------------------------------------------*/
-/* Function to find maximum between two numbers.*/
+/* 01. Write a ts program to find maximum between two numbers.*/
 /* ---------------------------------------------*/
 function maxNumberBT2(x: number, y: number) {
     if (x > y) {
@@ -30,7 +30,7 @@ form.addEventListener("submit", (e: Event) => {
 
 
 /* ---------------------------------------------*/
-/* Function to find maximum between three numbers.*/
+/* 02. Write a ts program to find maximum between three numbers.*/
 /* ---------------------------------------------*/
 
 /* Get form Elements*/
@@ -79,13 +79,13 @@ form02.addEventListener("submit", (e: Event) => {
 })
 
 /* ---------------------------------------------*/
-/* program to check whether a number is negative, positive or zero.*/
+/* 03. program to check whether a number is negative, positive or zero.*/
 /* ---------------------------------------------*/
 
 var problem03 = document.querySelector('#problem03')! as HTMLFormElement
 var numPoN = document.querySelector("#PoN")! as HTMLInputElement
 
-function postiveNumber(a:number) {
+function postiveNumber(a: number) {
     if (a > 0) {
         return (
             console.log('The Number is POSITIVE')
@@ -105,57 +105,251 @@ function postiveNumber(a:number) {
 problem03.addEventListener("submit", (e: Event) => {
     e.preventDefault()
     /* Print the output in cosole */
-    var pos = postiveNumber(numPoN.valueAsNumber)
-    console.log(pos)
+    postiveNumber(numPoN.valueAsNumber)
 })
 
 /* ---------------------------------------------*/
-/* 4. Write a ts program to check whether a number is divisible by 5 and 11 or not.*/
+/* 05. 5. Write a ts program to check whether a number is even or odd.*/
+/* ---------------------------------------------*/
+
+var problem05 = document.querySelector('#problem05')! as HTMLFormElement
+var input05 = document.querySelector('#input05')! as HTMLInputElement
+
+function isEvenOdd(x: number) {
+    if (x !== 0) {
+        if (x % 2 == 0) {
+            return (
+                console.log(`The Number (${x}) is EVEN`)
+            )
+        }
+        else {
+            console.log(`The Number (${x}) is ODD`)
+        }
+    }else{console.log('Please type a number greater than 0')}
+}
+
+problem05.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    isEvenOdd(input05.valueAsNumber)
+})
+
+/* ---------------------------------------------*/
+/* 04. Write a ts program to check whether a number is divisible by 5 and 11 or not.*/
 /* ---------------------------------------------*/
 
 var problem04 = document.querySelector('#problem04')! as HTMLFormElement
 var divideTo = document.querySelector('#divideTo')! as HTMLInputElement
 var divideBy = document.querySelector('#divideBy')! as HTMLInputElement
 
-function divisible(x:number, y:number){
-    if(x % y == 0){
-        return(
+function divisible(x: number, y: number) {
+    if (x % y == 0) {
+        return (
             console.log('The Number is Divisible')
         )
     }
-    else{
+    else {
         console.log('The Number is not Divisible')
     }
 }
 
-problem04.addEventListener("submit", (e:Event) => {
+problem04.addEventListener("submit", (e: Event) => {
     e.preventDefault()
-    console.log(
-        divisible(divideTo.valueAsNumber, divideBy.valueAsNumber)
-    )
+    divisible(divideTo.valueAsNumber, divideBy.valueAsNumber)
 })
 
 /* ---------------------------------------------*/
-/* 6. Write a ts program to check whether a year is leap year or not. */
+/* 06. Write a ts program to check whether a year is leap year or not. */
 /* ---------------------------------------------*/
 
 var problem06 = document.querySelector('#problem06')! as HTMLFormElement
 var year = document.querySelector('#year')! as HTMLInputElement
 
-function leapYear(x:number){
-    if(x % 4 == 0){
-        return(
-            console.log(`The Year ${x} is Leap Year` )
-            )
-        }
-        else{
-        console.log(`The Year ${x} is not Leap Year` )
+function leapYear(x: number) {
+    if (x % 4 == 0) {
+        return (
+            console.log(`The Year ${x} is Leap Year`)
+        )
+    }
+    else {
+        console.log(`The Year ${x} is not Leap Year`)
     }
 }
-problem06.addEventListener("submit", (e:Event) => {
+problem06.addEventListener("submit", (e: Event) => {
     e.preventDefault()
     console.log(
         leapYear(year.valueAsNumber)
     )
 })
+
+/* ---------------------------------------------*/
+/* 07. Write a ts program to check whether a character is alphabet or not. */
+/* ---------------------------------------------*/
+
+var problem07 = document.querySelector('#problem07')! as HTMLFormElement
+var character = document.querySelector('#character')! as HTMLInputElement
+
+function checkCharacter(x: string | null) {
+    if (x == x.toUpperCase()) {
+        return (
+            console.log(`The Character (${x}) is in Uppercase`)
+        )
+    }
+    else if (x == x.toLowerCase()) {
+        return (
+            console.log(`The Character (${x}) is in Lowercase`)
+        )
+    }
+    else {
+        return (
+            console.log("This is not a Character. Please type a Character")
+        )
+    }
+}
+problem07.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    checkCharacter(character.value)
+})
+
+
+/* ---------------------------------------------*/
+/* 08. Write a ts program to input any alphabet and check whether it is vowel or consonant.*/
+/* ---------------------------------------------*/
+
+var problem08 = document.querySelector('#problem08')! as HTMLFormElement
+var input08 = document.querySelector('#input08')! as HTMLInputElement
+
+function isVowel(x: any) {
+
+    var charLen = x.length;
+    var char: any;
+    var vowels: any = "aeiouAEIOU";
+
+    // console.log('Charactor Lenth', charLen);
+
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in vowels) {
+            if (vowels[char] == x) {
+                // console.log(n.length);
+                var vowelOut = vowels[char]
+                console.log(`The Charactor (${vowelOut}) is a VOWEL Charactor`);
+                break;
+            }
+        }
+        if (!(vowelOut)) {
+            console.log(`The Charactor (${x}) is not a VOWEL Charactor`)
+        }
+    }
+    else { console.log('Please Write only one Charactor') }
+
+}
+
+problem08.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    isVowel(input08.value)
+})
+
+/* ---------------------------------------------*/
+/* 09. Write a ts program to input any character and check whether it is alphabet, digit or special character.*/
+/* ---------------------------------------------*/
+var problem09 = document.querySelector('#problem09')! as HTMLFormElement
+var input09 = document.querySelector('#input09')! as HTMLInputElement
+
+function whatChar(x: any) {
+
+    var charLen = x.length;
+    var char: any;
+    var alphbets: any = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers: any = "0123456789";
+    // var spacialChar: any = "0123456789";
+
+    // console.log('Charactor Lenth', charLen);
+
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in alphbets) {
+            if (alphbets[char] == x) {
+                // console.log(n.length);
+                var alphbetOut = alphbets[char]
+                console.log(`The Charactor (${alphbetOut}) is an Alphabet`);
+                break
+            }
+        }
+        for (char in numbers) {
+            if (numbers[char] == x) {
+                // console.log(n.length);
+                var numberOut = numbers[char]
+                console.log(`The Charactor (${numberOut}) is a NUMBER`);
+                break
+            }
+        }
+        if (!(alphbetOut || numberOut)) {
+            console.log(`The input (${x}) is a Spacial Charactor `)
+        }
+
+    }
+    else { console.log('Please Write only one Charactor') }
+
+}
+
+problem09.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    whatChar(input09.value)
+
+})
+
+/* ---------------------------------------------*/
+/* 10. Write a ts program to check whether a character is uppercase or lowercase alphabet. */
+/* ---------------------------------------------*/
+
+var problem07B = document.querySelector('#problem07B')! as HTMLFormElement
+var characterB = document.querySelector('#characterB')! as HTMLInputElement
+
+function isLowerUpper(x: any) {
+
+    var charLen = x.length;
+    var char: any;
+    var lower: any = "abcdefghijklmnopqrstuvqxyz";
+    var upper: any = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // console.log('Charactor Lenth', charLen);
+
+    if (charLen == 1) {
+        // console.log('Input Lenth is OK')
+        for (char in lower) {
+            if (lower[char] == x) {
+                // console.log(n.length);
+                var lowerOut = lower[char]
+                console.log(`The Charactor (${lowerOut}) is an Alphabet and in LOWERCASE`);
+                break
+            }
+        }
+        for (char in upper) {
+            if (upper[char] == x) {
+                // console.log(n.length);
+                var upperOut = upper[char]
+                console.log(`The Charactor (${upperOut}) is an Alphabet and in UPPERCASE`);
+                break
+            }
+        }
+        if (!(lowerOut || upperOut)) {
+            console.log(`The input is not a charactor (${x}), Please Write a Alphabet charactor`)
+        }
+
+    }
+    else { console.log('Please Write only one Charactor') }
+
+}
+
+problem07B.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    isLowerUpper(characterB.value)
+
+})
+
+
+
+/* ---------------------------------------------*/
+/* 11. Write a ts program to input week number and print week day.*/
+/* ---------------------------------------------*/
 
